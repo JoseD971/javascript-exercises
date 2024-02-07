4fb44479820eaf33f4707698dc32eeb28a33a8fa
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2024-02-07 15:45:24 -0500

    Se modificaron los archivos .js (no .spec.js) de los ejercicios 01 hasta el 07.
    Las modificaciones solo dan solución al primer test de sus respectivos archivos .spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var complete_string = "";
+    if (num == 0) {
+        return "Error, enter a number greater than zero"
+    } else {
+        for (let i = 0; i < num; i++) {
+            complete_string = complete_string + string;
+        } 
+    }
+    return complete_string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, rem) {
+    var index = array.indexOf(rem);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
